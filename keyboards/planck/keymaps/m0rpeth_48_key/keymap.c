6ebab52acf6674e78c82695871f0b1653bdd6a6a
m0rpeth <m0rpeth@pm.me>
2023-03-17 21:16:16 +0100

    [Planck] Minor tweaks

--- keyboards/planck/keymaps/m0rpeth_48_key/keymap.c
+++ keyboards/planck/keymaps/m0rpeth_48_key/keymap.c
@@ -34,7 +34,10 @@ void leader_end_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case LT(0, KC_LWR):
-        if (record->event.pressed) {
+        if (record->tap.count && record->event.pressed && layer_state_is(LYR_RAISE)) {
+            // on tap
+            tap_code16(KC_GT);
+        } else if (record->event.pressed) {
             // on hold
             layer_on(LYR_LOWER);
             update_tri_layer(LYR_LOWER, LYR_RAISE, LYR_ADJUST);
@@ -55,54 +58,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             update_tri_layer(LYR_LOWER, LYR_RAISE, LYR_ADJUST);
         }
         return false;
-    case LCTL_T(KC_EXLM):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_EXLM); // Send KC_EXLM on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
-    case LALT_T(KC_AMPR):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_AMPR); // Send KC_AMPR on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
-    case LGUI_T(KC_LPRN):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_LPRN); // Send KC_LPRN on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
-    case LSFT_T(KC_RPRN):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_RPRN); // Send KC_RPRN on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
-    case RSFT_T(KC_MINS):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_MINS); // Send KC_MINS on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
-    case RGUI_T(KC_EQL):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_EQL); // Send KC_EQL on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
-    case RALT_T(KC_PLUS):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_PLUS); // Send KC_PLUS on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
-    case RCTL_T(KC_AT):
-        if (record->tap.count && record->event.pressed) {
-            tap_code16(KC_AT); // Send KC_AT on tap
-            return false;        // Return false to ignore further processing of key
-        }
-        break;
     }
   return true;
 }
@@ -124,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LYR_ALPHA] = LAYOUT_ortho_4x12(
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
-        KC_LCTL, KC_NO, QK_LALT, KC_LGUI, LT(0, KC_LWR), KC_SPC, KC_SPC, LT(0, KC_RSE), KC_ENT, KC_NO, QK_LEAD, KC_DEL
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_LALT, KC_LGUI, QK_LEAD, LT(0, KC_LWR), KC_SPC, KC_SPC, LT(0, KC_RSE), KC_ENT, KC_RGUI, KC_RALT, KC_RCTL
     ),
     [LYR_COLEMAK] = LAYOUT_ortho_4x12(
         KC_TRNS, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_TRNS,
@@ -134,16 +89,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [LYR_LOWER] = LAYOUT_ortho_4x12(
-        KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_TRNS,
-        KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_TRNS
+        KC_NO, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_NO, KC_NO, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+        KC_NO, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO,
+        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [LYR_RAISE] = LAYOUT_ortho_4x12(
-        KC_CIRC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DLR,
+        KC_TILD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
         KC_HASH, KC_EXLM, KC_AMPR, KC_LPRN, KC_RPRN, KC_GRV, KC_PIPE, KC_MINS, KC_EQL, KC_PLUS, KC_COLN, KC_DQUO,
-        KC_LT, KC_QUES, KC_AT, KC_LCBR, KC_RCBR, KC_PERC, KC_BSLS, KC_LBRC, KC_RBRC, KC_ASTR, KC_UNDS, KC_GT,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO
+        KC_CIRC, KC_QUES, KC_AT, KC_LCBR, KC_RCBR, KC_PERC, KC_ASTR, KC_LBRC, KC_RBRC, KC_BSLS, KC_UNDS, KC_DLR,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_LT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [LYR_ADJUST] = LAYOUT_ortho_4x12(
         KC_NO, KC_NO, KC_NO, AG_SWAP, AG_NORM, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT,
