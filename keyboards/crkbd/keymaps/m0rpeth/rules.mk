5f09a5b4267cc10ffd83ffa69ea80b629651d8b0
m0rpeth <m0rpeth@pm.me>
2024-01-21 22:24:24 +0100

    Add corne layout

---
+++ keyboards/crkbd/keymaps/m0rpeth/rules.mk
@@ -0,0 +1,26 @@
+OLED_ENABLE = yes
+OLED_TIMEOUT = 0
+OLED_DRIVER = SSD1306
+
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SLEEP_LED_ENABLE = no
+AUDIO_ENABLE = no
+BOOTMAGIC_ENABLE = no
+LEADER_ENABLE = yes
+
+# Backlight: Single color, per key, not individually addressable (all leds on or all leds off)
+# RGBLight: Underglow RGB, linear design (single chain), individually addressable
+# LED Matrix: Single color, per key, individually addressable
+# RGB Matrix: RGB, per key, individually addressable
+# Indicators: Scroll/Caps/Num Lock LEDs
+
+RGBLIGHT_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
+RGB_MATRIX_ENABLE = no
+
+LTO_ENABLE = yes
+VIA_ENABLE = no
+#COMBO_ENABLE = yes
+
