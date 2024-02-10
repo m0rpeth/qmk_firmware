ad648b39dfd9c665ca8baa8e81275708a10ded03
m0rpeth <m0rpeth@pm.me>
2024-02-10 01:48:25 +0100

    Update Dactyl_CC Keymap

--- keyboards/handwired/dactyl_cc/keymaps/m0rpeth/keymap.c
+++ keyboards/handwired/dactyl_cc/keymaps/m0rpeth/keymap.c
@@ -2,20 +2,12 @@
 
 enum layers {
     LYR_QWERTY = 0,
-    LYR_LOWER,
-    LYR_RAISE,
+    LYR_NAV,
+    LYR_SYM,
     LYR_FN,
-    LYR_ADJUST,
 };
 
-enum custom_keycodes {
-	KC_LWR = SAFE_RANGE,
-	KC_RSE,
-	KC_ADJUST,
-};
-
-#define LT_LWR LT(0, KC_LWR)
-#define LT_RSE LT(0, KC_RSE)
+// screenshot macro?
 
 void leader_end_user(void) {
     if (leader_sequence_one_key(KC_M)) {
@@ -56,41 +48,50 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_BSPC);
             }
             return false;
-
-        case LT_LWR:
-            if (record->event.pressed) {
-                // on hold
-                layer_on(LYR_LOWER);
-                update_tri_layer(LYR_LOWER, LYR_RAISE, LYR_ADJUST);
-            } else {
-                // on release
-                layer_off(LYR_LOWER);
-                update_tri_layer(LYR_LOWER, LYR_RAISE, LYR_ADJUST);
-            }
-            return false;
-
-        case LT_RSE:
-            if (record->event.pressed) {
-                // on hold
-                layer_on(LYR_RAISE);
-                update_tri_layer(LYR_LOWER, LYR_RAISE, LYR_ADJUST);
-            } else {
-                // on release
-                layer_off(LYR_RAISE);
-                update_tri_layer(LYR_LOWER, LYR_RAISE, LYR_ADJUST);
-            }
-            return false;
     }
     return true;
 };
 
+#define TO_NAV LT(LYR_NAV, KC_SPC)
+#define TO_SYM LT(LYR_SYM, KC_ESC)
+#define TO_FN OSL(LYR_FN)
+#define CK_ENT RGUI_T(KC_ENT)
+#define CK_LSFT LSFT_T(KC_F)
+#define CK_RSFT RSFT_T(KC_J)
+#define CK_STAB RSFT(KC_TAB)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LYR_QWERTY] = LAYOUT(
-        KC_EQL,     KC_1,    KC_2,      KC_3,       KC_4,       KC_5,                   KC_6,       KC_7,       KC_8,       KC_9,      KC_0,       KC_MINS,
-        KC_TAB,     KC_Q,    KC_W,      KC_E,       KC_R,       KC_T,                   KC_Y,       KC_U,       KC_I,       KC_O,      KC_P,       KC_BSLS,
-        KC_ESC,     KC_A,    KC_S,      KC_D,       KC_F,       KC_G,                   KC_H,       KC_J,       KC_K,       KC_L,      KC_SCLN,    KC_QUOT,
-        KC_LSFT,    KC_Z,    KC_X,      KC_C,       KC_V,       KC_B,                   KC_N,       KC_M,       KC_COMM,    KC_DOT,    KC_SLSH,    KC_RSFT,
-        KC_NO,      KC_GRV,  KC_CAPS,   KC_LEFT,    KC_RGHT,    KC_LCTL,                KC_RCTL,    KC_UP,      KC_DOWN,    KC_LBRC,   KC_RBRC,    KC_NO,
-        KC_NO,      KC_BSPC, KC_DEL,    KC_END,     KC_HOME,    KC_LALT,                KC_RGUI,    KC_PGUP,    KC_PGDN,    KC_ENT,    KC_SPC,     KC_NO
+        KC_GRV,     KC_1,    KC_2,      KC_3,       KC_4,       KC_5,                   KC_6,       KC_7,       KC_8,       KC_9,      KC_0,       KC_MINS,
+        KC_ESC,     KC_Q,    KC_W,      KC_E,       KC_R,       KC_T,                   KC_Y,       KC_U,       KC_I,       KC_O,      KC_P,       KC_DEL,
+        KC_TAB,     KC_A,    KC_S,      KC_D,       CK_LSFT,    KC_G,                   KC_H,       CK_RSFT,    KC_K,       KC_L,      KC_SCLN,    KC_QUOT,
+        TO_FN,      KC_Z,    KC_X,      KC_C,       KC_V,       KC_B,                   KC_N,       KC_M,       KC_COMM,    KC_DOT,    KC_SLSH,    TO_FN,
+
+        KC_NO,      KC_LCTL, KC_LSFT,   KC_LALT,    KC_LGUI,    CK_STAB,                KC_NO,      KC_RGUI,    KC_RALT,    KC_RSFT,   KC_RCTL,    KC_NO,
+        KC_NO,      KC_BSPC, TO_NAV,    KC_LSFT,    KC_NO,      KC_TAB,                 KC_NO,      KC_NO,      KC_RSFT,    TO_SYM,    CK_ENT,     KC_NO
+    ),
+    [LYR_SYM] = LAYOUT(
+        KC_NO,      KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_NO,
+        KC_NO,      KC_NO,   KC_PERC,   KC_LBRC,    KC_RBRC,    KC_GRV,                 KC_CIRC,    KC_SLSH,    KC_DLR,     KC_ASTR,   KC_HASH,    KC_NO,
+        KC_NO,      KC_QUES, KC_EXLM,   KC_LPRN,    KC_RPRN,    KC_PIPE,                KC_AT,      KC_MINS,    KC_EQL,     KC_PLUS,   KC_COLN,    KC_NO,
+        KC_NO,      KC_AMPR, KC_UNDS,   KC_LCBR,    KC_RCBR,    KC_NO,                  KC_BSLS,    KC_TILD,    KC_LT,      KC_GT,     KC_DQUO,    KC_NO,
+        KC_NO,      KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_NO,
+        KC_NO,      KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_NO
+    ),
+    [LYR_NAV] = LAYOUT(
+        KC_NO,      KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_NO,
+        KC_NO,      KC_NO,   KC_NO,     KC_NO,      KC_NO,      KC_NO,                  KC_HOME,    KC_PGUP,    KC_PGDN,    KC_END,    KC_NO,      KC_NO,
+        KC_NO,      KC_NO,   KC_NO,     KC_NO,      KC_NO,      KC_NO,                  KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,  KC_NO,      KC_NO,
+        KC_NO,      KC_NO,   KC_NO,     KC_NO,      KC_NO,      KC_NO,                  KC_NO,      KC_NO,      KC_NO,      KC_NO,     KC_NO,      KC_NO,
+        KC_NO,      KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_NO,
+        KC_NO,      KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_NO
+    ),
+    [LYR_FN] = LAYOUT(
+        KC_NO,      KC_NO,   KC_NO,     KC_NO,      KC_NO,      KC_NO,                  KC_NO,      KC_NO,      KC_NO,      KC_NO,     KC_NO,      QK_BOOT,
+        KC_NO,      KC_NO,   KC_NO,     KC_NO,      KC_NO,      KC_NO,                  KC_NO,      KC_F9,      KC_F10,     KC_F11,    KC_F12,     KC_NO,
+        KC_NO,      KC_NO,   KC_NO,     KC_NO,      KC_NO,      KC_NO,                  KC_NO,      KC_F5,      KC_F6,      KC_F7,     KC_F8,      KC_NO,
+        KC_NO,      KC_NO,   KC_NO,     KC_NO,      KC_NO,      KC_NO,                  KC_NO,      KC_F1,      KC_F2,      KC_F3,     KC_F4,      KC_NO,
+        KC_NO,      KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_NO,
+        KC_NO,      KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_NO
     )
 };
