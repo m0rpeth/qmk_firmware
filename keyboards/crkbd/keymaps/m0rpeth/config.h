c0f630b44bf4faa1c3667da13c2e1513e47d0509
m0rpeth <m0rpeth@pm.me>
2024-02-17 22:32:09 +0100

    Update corne keymap

--- keyboards/crkbd/keymaps/m0rpeth/config.h
+++ keyboards/crkbd/keymaps/m0rpeth/config.h
@@ -2,15 +2,15 @@
 
 #define OLED_DISPLAY_128x32
 
-#define ONESHOT_TAP_TOGGLE 20                       // Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 5000                        // Time (in ms) before the one shot key is released */
+#define ONESHOT_TAP_TOGGLE 20 // Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  // Time (in ms) before the one shot key is released */
 
-#define MK_3_SPEED                                  // constant mouse-speed mode, no acceleration
+#define MK_3_SPEED // constant mouse-speed mode, no acceleration
 
 #define COMBO_TERM 50
 #define COMBO_MUST_HOLD_MODS
 
-//#define QUICK_TAP_TERM ???
+// #define QUICK_TAP_TERM ???
 
 #define TAPPING_TERM 190
 #define PERMISSIVE_HOLD
@@ -18,18 +18,16 @@
 #define LEADER_PER_KEY_TIMING
 #define LEADER_TIMEOUT 1000
 
-#define AUTO_SHIFT_TIMEOUT 165
+// #define AUTO_SHIFT_TIMEOUT 165
+// #define NO_AUTO_SHIFT_ALPHA                         // disallow alphas to be auto shifted
+// #define NO_AUTO_SHIFT_NUMERIC                     // disallow 1234567890 to be auto shifted
+// #define NO_AUTO_SHIFT_SPECIAL                     // disallow -_, =+, [{, ]}, ;:, ‘“, ,<, .>, and /? to be auto shifted
 
-//#define NO_AUTO_SHIFT_ALPHA                         // disallow alphas to be auto shifted
-//#define NO_AUTO_SHIFT_NUMERIC                     // disallow 1234567890 to be auto shifted
-//#define NO_AUTO_SHIFT_SPECIAL                     // disallow -_, =+, [{, ]}, ;:, ‘“, ,<, .>, and /? to be auto shifted
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
 //
 // save some space ...
 //
 #ifndef NO_DEBUG
-#define NO_DEBUG
+#    define NO_DEBUG
 #endif
-
-#define SPLIT_LAYER_STATE_ENABLE
-#define IS_RIGHT_SIDE_FIRMWARE
--- keyboards/crkbd/keymaps/m0rpeth/rules.mk
+++ keyboards/crkbd/keymaps/m0rpeth/rules.mk
@@ -1,13 +1,13 @@
 OLED_ENABLE = yes
 OLED_TIMEOUT = 0
-OLED_DRIVER = SSD1306
+OLED_DRIVER = ssd1306
 
 MOUSEKEY_ENABLE = no
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 SLEEP_LED_ENABLE = no
 AUDIO_ENABLE = no
-BOOTMAGIC_ENABLE = no
+BOOTMAGIC_ENABLE = yes
 LEADER_ENABLE = yes
 
 # Backlight: Single color, per key, not individually addressable (all leds on or all leds off)
@@ -20,7 +20,6 @@ RGBLIGHT_SUPPORTED = no
 RGB_MATRIX_SUPPORTED = no
 RGB_MATRIX_ENABLE = no
 
-LTO_ENABLE = yes
 VIA_ENABLE = no
 #COMBO_ENABLE = yes
 
