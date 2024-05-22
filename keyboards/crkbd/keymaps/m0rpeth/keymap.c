f0562ab5bbdbdcfaba3a4a80553ab1cbe3fb33c5
m0rpeth <m0rpeth@pm.me>
2024-05-22 02:34:07 +0200

    Small fixes

--- keyboards/crkbd/keymaps/m0rpeth/keymap.c
+++ keyboards/crkbd/keymaps/m0rpeth/keymap.c
@@ -1,3 +1,5 @@
+#include QMK_KEYBOARD_H
+
 enum layers {
     LYR_QWERTY = 0,
     LYR_NAV,
@@ -63,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,     KC_Q,    KC_W,    KC_E,      KC_R,      KC_T,              KC_Y,      KC_U,      KC_I,       KC_O,      KC_P,       KC_BSPC,
         KC_TAB,     KC_A,    KC_S,    KC_D,      KC_F,      KC_G,              KC_H,      KC_J,      KC_K,       KC_L,      KC_SCLN,    KC_QUOT,
         KC_LSFT,    KC_Z,    KC_X,    KC_C,      KC_V,      KC_B,              KC_N,      KC_M,      KC_COMM,    KC_DOT,    KC_SLSH,    KC_RSFT,
-                                      MT_ESC,    TO_NAV,    KC_SPC,            KC_SPC,    TO_SYM,    MT_ENT
+                                      MT_ESC,    LT_NAV,    KC_SPC,            KC_SPC,    LT_SYM,    MT_ENT
     ),
 
     [LYR_NAV] = LAYOUT_split_3x6_3(
@@ -85,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_F5,    KC_F6,     KC_F7,     KC_F8,     KC_NO,
         KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_F1,    KC_F2,     KC_F3,     KC_F4,     KC_NO,
                                         KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS
-    )
+    ),
 
     [LYR_ADJ] = LAYOUT_split_3x6_3(
         KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_NO,    KC_NO,      KC_NO,       KC_NO,      KC_NO,      QK_BOOT,
